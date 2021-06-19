0b2278b48ed253d32718de2536f0c302d2786406
Ravikumar C S <raviroyalmech233@gmail.com>
2021-06-19 22:29:30 +0530

    added url

--- src/app/service/user-details.service.ts
+++ src/app/service/user-details.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { myurlnodeUrl } from '../urls/url';
 import { Observable } from 'rxjs';
-
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Access-Control-Allow-Origin': '*',
+    'Authorization': 'authkey',
+    'userid': '1'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -12,14 +18,14 @@ export class UserDetailsService {
   constructor(private http: HttpClient) { }
 
   getUserDetails(): Observable<any> {
-    return this.http.get(myurlnodeUrl + '/user');
+    return this.http.get(myurlnodeUrl + '/user', httpOptions);
   }
 
   postUserDetails(payload): Observable<any> {
-    return this.http.post(myurlnodeUrl + '/user', payload);
+    return this.http.post(myurlnodeUrl + '/user', payload, httpOptions);
   }
 
   deleteUserDetails(id): Observable<any> {
-    return this.http.delete(myurlnodeUrl + '/user/' + id);
+    return this.http.delete(myurlnodeUrl + '/user/' + id, httpOptions);
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  url: 'http://localhost:3000'
+  url: 'https://node-a60mv3avr-ravi-royal-143.vercel.app'
 };
 
 /*

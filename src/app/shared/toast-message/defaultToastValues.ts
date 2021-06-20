89bea08b16b3ca24f8f33372e789c6c4d7296be9
Ravikumar C S <raviroyalmech233@gmail.com>
2021-06-20 13:52:22 +0530

    updated toast

---
+++ src/app/shared/toast-message/defaultToastValues.ts
@@ -0,0 +1,6 @@
+import { Message } from "primeng/api";
+
+export const defaultToast: Message = {
+    key: 'royal',
+    life: 5000
+}
--- src/app/shared/toast-message/toast-message.service.ts
+++ src/app/shared/toast-message/toast-message.service.ts
@@ -1,18 +1,41 @@
 import { Injectable } from '@angular/core';
-import {MessageService} from 'primeng/api';
+import { Message, MessageService } from 'primeng/api';
+import { defaultToast } from './defaultToastValues';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ToastMessageService {
 
-  constructor(private messageService: MessageService) {}
+  constructor(private messageService: MessageService) { }
 
   addToast(data): void {
-    this.messageService.add({severity: data.severity, summary: data.summary, detail: data.detail});
+    this.messageService.add({ severity: data.severity, summary: data.summary, detail: data.detail });
   }
+
+  multiplyToast(toasts: Message[]) {
+    const toast = toasts.map(singleToast => ({
+      ...defaultToast,
+      ...singleToast
+    }));
+
+    if (toast.length > 0) {
+      this.messageService.addAll(toast);
+    }
+  }
+
   clear(): void {
     this.messageService.clear();
+  }
+
+  showSuccessToast(details: string[], key: string = defaultToast.key) {
+    const toastMes = details.map(detail => ({ key, severity: 'success', summary: '', detail }));
+    this.multiplyToast(toastMes);
+  }
+
+  showErrorToast(details: string[], key: string = defaultToast.key) {
+    const toastMes = details.map(detail => ({ key, severity: 'error', summary: '', detail }));
+    this.multiplyToast(toastMes);
   }
 
 }

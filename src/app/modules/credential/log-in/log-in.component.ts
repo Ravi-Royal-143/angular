2c97c6c8e9fae71bd82fbb67182abbb43b793344
Ravikumar C S <raviroyalmech233@gmail.com>
2021-06-25 11:11:55 +0530

    added log in validation

--- src/app/modules/credential/log-in/log-in.component.ts
+++ src/app/modules/credential/log-in/log-in.component.ts
@@ -10,11 +10,25 @@ import { LogInService } from './log-in/log-in.service';
   styleUrls: ['./log-in.component.scss']
 })
 export class LogInComponent {
-
+  isgmailValidate: boolean;
+  ispasswordValidate: boolean;
   userInfo: FormGroup = this.fb.group({
-    gmail: ['', Validators.required],
+    gmail: ['', [Validators.required, Validators.email]],
     password: ['', Validators.required]
   });
+
+  get formDetails() {
+    return this.userInfo.controls;
+  }
+  
+  get gmailFormDetails() {
+    return this.formDetails.gmail;
+  }
+
+  get passwordFormDetails() {
+    return this.formDetails.password;
+  }
+
 
   constructor(
     private fb: FormBuilder,
@@ -24,12 +38,26 @@ export class LogInComponent {
   ) { }
 
   onSubmit() {
+    this.checkOnSubmit();
     const { gmail, password } = this.userInfo.value;
+    let isFieldInvalid = false;
+    if (this.userInfo.invalid) {
+      return;
+    }
     this.logInService.authenticate({ gmail, password }).subscribe(res => {
       document.cookie = 'cookie' + '=' + res.cookie;
       this.toastMessageService.showSuccessToast(['Sucessfully logged in.']);
       this.router.navigate(['/']);
     });
+  }
+
+  onBlurFields(validateField) {
+    this[validateField] = true;
+  }
+
+  checkOnSubmit() {
+    this.isgmailValidate = true;
+    this.ispasswordValidate = true;
   }
 
 }
